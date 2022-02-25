16a0d0940a9a838b020a1aaabeb12b87303990de
Bryce <bryceohmer@hotmail.com>
2022-02-25 11:06:43 -0500

    Added things to automatically fetch weather data, what is promise hell?

--- src/index.js
+++ src/index.js
@@ -1,47 +1,28 @@
 import fetch from 'node-fetch';
 import express from 'express';
-import bodyParser from 'body-parser';
 import cron from 'node-cron';
 import { MongoClient } from 'mongodb';
+import 'dotenv/config';
 
 const app = express();
-const mongoUri = 'mongodb://localhost:27017';
+const mongoUri = process.env.MONGO_URI;
 const client = new MongoClient(mongoUri);
-
 const dbName = 'bryceohmer';
 
-app.get('/api/weather', (req, res) => {
-    fetch('http://api.openweathermap.org/data/2.5/weather?zip=28461&appid=12a6b20d756c5b22ec58b419b759d177')
-        .then((res) => res.json())
-        .then((data) => {
-            data.weather[0].iconUrl = `http://openweathermap.org/img/wn/${data.weather[0].icon}@4x.png`;
-            res.send(data);
-        });
-});
-
-async function fetching() {
-    await client.connect();
-    console.log('connected');
-    const db = client.db(dbName);
-    const collection = db.collection('testing');
-    const findResult = await collection.find({}).toArray();
-    console.log('Found documents =>', findResult);
-
-    return 'done';
-}
-
-const fetcher = () => {
+app.get('/api/currentWeather', (req, res) => {
     client.connect()
         .then((cl) => {
             const db = cl.db(dbName);
-            const collection = db.collection('testing');
-            collection.find({}).toArray()
-                .then((data) => console.log('Found dockies => ', data));
+            const collection = db.collection('currentWeather');
+            collection.find({}).limit(1).sort({_id: -1}).toArray()
+                .then((data) => res.send(data));
         });
-};
+});
 
+// For testing
 // cron.schedule(`*/2 * * * * *`, () => {
-//     saveCurrentWeather();
+//     console.log('doing the dang thang')
+//     saveHistorical()
 // })
 
 //Fetch current weather every hour
@@ -49,24 +30,62 @@ cron.schedule(`0 * * * *`, () => {
     saveCurrentWeather();
 });
 
-//Fetch forecast once a day at 8am
+//Fetch forecast and historical once a day at 8am
 cron.schedule(`0 8 * * *`, () => {
-
+    saveForecast();
+    saveHistorical();
 });
 
-async function saveCurrentWeather() {
-    fetch('http://api.openweathermap.org/data/2.5/weather?zip=28461&appid=12a6b20d756c5b22ec58b419b759d177')
+function saveCurrentWeather() {
+    fetch(`http://api.openweathermap.org/data/2.5/weather?zip=28461&appid=${process.env.WEATHER_KEY}`)
         .then((res) => res.json())
         .then((data) => {
             data.weather[0].iconUrl = `http://openweathermap.org/img/wn/${data.weather[0].icon}@4x.png`;
+            data.zip = 28461;
 
             client.connect()
                 .then((cl) => {
                     const db = cl.db(dbName);
-                    const collection = db.collection('weather');
+                    const collection = db.collection('currentWeather');
                     collection.insertOne(data);
                 });
         });
+}
+
+function saveHistorical() {
+    client.connect()
+        .then((cl) => {
+            const db = cl.db(dbName);
+            const collection = db.collection('currentWeather');
+            collection.findOne({zip: 28461})
+                .then((found) => {
+                    fetch(`http://api.openweathermap.org/data/2.5/onecall/timemachine?lat=${found.coord.lat}&lon=${found.coord.lon}&dt=${Math.floor(new Date().getTime() / 1000) - 43200}&appid=${process.env.WEATHER_KEY}`)
+                        .then((res) => res.json())
+                        .then((data) => {
+                            data.zip = 28461;
+
+                            const collection = db.collection('weatherHistory');
+                            collection.insertOne(data);
+                        });
+                });
+        });
+}
+
+function saveForecast() {
+    fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=33.9357466&lon=-78.0546333&exclude=minutely,hourly,current,alerts&appid=${process.env.WEATHER_KEY}`)
+        .then((res) => res.json())
+        .then((data => {
+            data.daily.forEach((w) => {
+                w.weather[0].iconUrl = `http://openweathermap.org/img/wn/${data.weather[0].icon}@4x.png`
+            });
+
+            client.connect()
+                .then((cl) => {
+                    const db = cl.db(dbName);
+                    const collection = db.collection('weatherForecast');
+                    collection.insertOne(data);
+                });
+        }))
 }
 
 app.listen(3001, () => {
